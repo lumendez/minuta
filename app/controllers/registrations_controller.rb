79a7686b684cb193ae24ae1f08484e06a0e8f370
luis <luis@localhost.localdomain>
2016-12-05 14:05:14 -0600

    Se agrego area a registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:nombre, :boleta, :sepi_programa_id, :consejero_id, :coordinador_nombre_id, :tipos_usuario_id, :email, :user_name, :password, :password_confirmation)
+    params.require(:user).permit(:nombre, :boleta, :sepi_programa_id, :consejero_id, :coordinador_nombre_id, :tipos_usuario_id, :email, :user_name, :password, :password_confirmation, :area)
   end
 
   def account_update_params
-    params.require(:user).permit(:nombre, :boleta, :sepi_programa_id, :consejero_id, :coordinador_nombre_id, :tipos_usuario_id, :email, :user_name, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:nombre, :boleta, :sepi_programa_id, :consejero_id, :coordinador_nombre_id, :tipos_usuario_id, :email, :user_name, :password, :password_confirmation, :current_password, :area)
   end
 end
